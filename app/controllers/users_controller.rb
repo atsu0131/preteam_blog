7b24a0e5db1bb67a9f369bf23954323566b523b6
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2018-09-17 17:20:46 +0900

    edit画面の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
   end
 
  private
