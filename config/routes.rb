499b00d4b1ec4a95e2f251c5451d0fb179502c04
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-09-17 11:04:43 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  root to: 'toppages#index'
+
+  resources :blogs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
