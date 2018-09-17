bb5c8a1ae60074a5dfddd403a847547b28a0cf0c
川田　京助 <kawatakyousuke@kawatakyousukenoMacBook-Pro.local>
2018-09-17 13:30:05 +0900

    バリテーション追加、gemファイル追加、アソシエーション設定（仮）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
+  has_many :blogs
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_blogs, through: :favorites, source: :blog
 end
