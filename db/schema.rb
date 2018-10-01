7e2693c9a835a2c71dec1b5c729209b471026325
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-10-01 14:19:44 +0900

    マイグレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180917111640) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +18,7 @@ ActiveRecord::Schema.define(version: 20180917111640) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "content"
-
-    t.bigint "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
@@ -30,7 +27,7 @@ ActiveRecord::Schema.define(version: 20180917111640) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "password_diggest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
